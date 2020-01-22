ea36cd512ca9bc8520d422120e8a71f8f5b31f0e
Agustin Carames <agustin.carames@creativecoefficient.net>
2020-01-22 16:01:17 -0300

    Fix to actually send access_token with SDResp

--- src/protocol/SelectiveDisclosureResponse.ts
+++ src/protocol/SelectiveDisclosureResponse.ts
@@ -148,7 +148,8 @@ export const SelectiveDisclosureResponse = {
 			method: "POST",
 			headers: {
 				"Content-Type": "application/json; charset=utf-8"
-			}
+			},
+			body: JSON.stringify({ access_token: args.token })
 		});
 	}
 };
