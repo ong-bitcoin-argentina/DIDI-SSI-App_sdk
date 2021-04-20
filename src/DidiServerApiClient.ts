ab1cbd9790d1d699220b0b10515c326a69e4419b
Diegoro21 <diero17@gmail.com>
2021-04-20 16:39:42 -0300

    Envio de salt por el constructor MM-158

--- src/DidiServerApiClient.ts
+++ src/DidiServerApiClient.ts
@@ -77,36 +77,36 @@ export type ApiResult<T> = Promise<Either<CommonServiceRequestError, T>>;
  */
 export class DidiServerApiClient {
 	private baseUrl: string;
+	private privateKeyPassword: string;
 
-	constructor(config: DidiServerApiClientConfiguration) {
+	constructor(config: DidiServerApiClientConfiguration, privateKey: string) {
 		this.baseUrl = config.didiServerUri;
+		this.privateKeyPassword = privateKey;
 	}
 
 	async changeEmail(
 		did: EthrDID,
 		validationCode: string,
 		newEmail: string,
-		password: string,
-		privateKeyPassword: string
+		password: string
 	): ApiResult<{ certificate: string }> {
 		return commonServiceRequest("POST", `${this.baseUrl}/changeEmail`, responseCodecs.singleCertificate, {
 			did: did.did(),
 			eMailValidationCode: validationCode,
 			newEMail: newEmail,
-			password: await Encryption.hash(password, privateKeyPassword)
+			password: await Encryption.hash(password, this.privateKeyPassword)
 		});
 	}
 
 	async changePassword(
 		did: EthrDID, 
 		oldPassword: string, 
-		newPassword: string,
-		privateKeyPassword: string,
+		newPassword: string
 	): ApiResult<{}> {
 		return commonServiceRequest("POST", `${this.baseUrl}/changePassword`, responseCodecs.empty, {
 			did: did.did(),
-			oldPass: await Encryption.hash(oldPassword, privateKeyPassword),
-			newPass: await Encryption.hash(newPassword, privateKeyPassword)
+			oldPass: await Encryption.hash(oldPassword, this.privateKeyPassword),
+			newPass: await Encryption.hash(newPassword, this.privateKeyPassword)
 		});
 	}
 
@@ -115,14 +115,13 @@ export class DidiServerApiClient {
 		validationCode: string,
 		newPhoneNumber: string,
 		password: string,
-		privateKeyPassword: string,
 		firebaseId?: string
 	): ApiResult<{ certificate: string }> {
 		return commonServiceRequest("POST", `${this.baseUrl}/changePhoneNumber`, responseCodecs.singleCertificate, {
 			did: did.did(),
 			phoneValidationCode: validationCode,
 			newPhoneNumber,
-			password: await Encryption.hash(password, privateKeyPassword),
+			password: await Encryption.hash(password, this.privateKeyPassword),
 			...(firebaseId ? { firebaseId } : {})
 		});
 	}
@@ -137,7 +136,6 @@ export class DidiServerApiClient {
 	async recoverAccount(
 		email: string,
 		password: string,
-		privateKeyPassword: string,
 		firebaseId?: string
 	): ApiResult<{ privateKeySeed: string }> {
 		const response = await commonServiceRequest(
@@ -146,7 +144,7 @@ export class DidiServerApiClient {
 			responseCodecs.accountRecovery,
 			{
 				eMail: email,
-				password: await Encryption.hash(password, privateKeyPassword),
+				password: await Encryption.hash(password, this.privateKeyPassword),
 				...(firebaseId ? { firebaseId } : {})
 			}
 		);
@@ -156,7 +154,7 @@ export class DidiServerApiClient {
 		}
 
 		try {
-			const privateKeySeed = await Encryption.decrypt(response.right.privateKeySeed, privateKeyPassword);
+			const privateKeySeed = await Encryption.decrypt(response.right.privateKeySeed, this.privateKeyPassword);
 			return right({ privateKeySeed });
 		} catch (error) {
 			log(error);
@@ -167,19 +165,17 @@ export class DidiServerApiClient {
 	async recoverPassword(
 		email: string, 
 		validationCode: string, 
-		newPassword: string,
-		privateKeyPassword: string
+		newPassword: string
 	): ApiResult<{}> {
 		return commonServiceRequest("POST", `${this.baseUrl}/recoverPassword`, responseCodecs.empty, {
 			eMail: email,
 			eMailValidationCode: validationCode,
-			newPass: await Encryption.hash(newPassword, privateKeyPassword)
+			newPass: await Encryption.hash(newPassword, this.privateKeyPassword)
 		});
 	}
 
 	async registerUser(
 		did: EthrDID,
-		privateKeyPassword: string,
 		userData: {
 			email: string;
 			phoneNumber: string;
@@ -191,12 +187,12 @@ export class DidiServerApiClient {
 		firebaseId?: string
 	): ApiResult<{}> {
 		try {
-			const encryptedPrivateKeySeed = await Encryption.encrypt(userData.privateKeySeed, privateKeyPassword);
+			const encryptedPrivateKeySeed = await Encryption.encrypt(userData.privateKeySeed, this.privateKeyPassword);
 			return commonServiceRequest("POST", `${this.baseUrl}/registerUser`, responseCodecs.empty, {
 				did: did.did(),
 				eMail: userData.email,
 				phoneNumber: userData.phoneNumber,
-				password: await Encryption.hash(userData.password, privateKeyPassword),
+				password: await Encryption.hash(userData.password, this.privateKeyPassword),
 				privateKeySeed: encryptedPrivateKeySeed,
 				name: userData.name,
 				lastname: userData.lastname,
@@ -222,7 +218,6 @@ export class DidiServerApiClient {
 	 */
 	async sendSmsValidator(
 		cellPhoneNumber: string,
-		privateKeyPassword: string,
 		idCheck?: {
 			did: EthrDID;
 			password: string;
@@ -234,7 +229,7 @@ export class DidiServerApiClient {
 			unique,
 			...(idCheck && {
 				did: idCheck.did.did(),
-				password: await Encryption.hash(idCheck.password, privateKeyPassword)
+				password: await Encryption.hash(idCheck.password, this.privateKeyPassword)
 			})
 		});
 	}
@@ -252,7 +247,6 @@ export class DidiServerApiClient {
 	 */
 	async sendMailValidator(
 		eMail: string,
-		privateKeyPassword: string,
 		idCheck?: {
 			did: EthrDID;
 			password: string;
@@ -264,7 +258,7 @@ export class DidiServerApiClient {
 			unique,
 			...(idCheck && {
 				did: idCheck.did.did(),
-				password: await Encryption.hash(idCheck.password, privateKeyPassword)
+				password: await Encryption.hash(idCheck.password, this.privateKeyPassword)
 			})
 		});
 	}
@@ -273,13 +267,12 @@ export class DidiServerApiClient {
 		did: EthrDID, 
 		email: string, 
 		password: string, 
-		privateKeyPassword: string,
 		firebaseId?: string
 	): ApiResult<{}> {
 		return commonServiceRequest("POST", `${this.baseUrl}/userLogin`, responseCodecs.empty, {
 			did: did.did(),
 			eMail: email,
-			password: await Encryption.hash(password, privateKeyPassword),
+			password: await Encryption.hash(password, this.privateKeyPassword),
 			...(firebaseId ? { firebaseId } : {})
 		});
 	}
