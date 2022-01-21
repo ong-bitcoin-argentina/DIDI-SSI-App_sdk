64c5588a33db358e91c935e0d1c4acf381e1ccd0
farpa <arpirez.fabio@gmail.com>
2022-01-21 15:18:41 -0300

    add mock jest and node-fetch

--- __tests__/test/VUSecurityApiClient-test.tsx
+++ __tests__/test/VUSecurityApiClient-test.tsx
@@ -1,18 +1,26 @@
 require('isomorphic-fetch');
+jest.mock('node-fetch');
+const fetch = jest.fn();
 import {VUSecurityApiClient} from '../../src/VUSecurityApiClient';
 
-describe("VUSecurityApiClient", () => {
+const responseRegisterUser = {}
+const responseCreateVerification = {"userName": "VU-TEST-SDK-1","operationId": 853}
+const responseCancelVerification = "Operacion cancelada exitosamente";
 
+
+describe("VUSecurityApiClient", () => {
    it(`registerUser`, async (done) => {
+      fetch.mockReturnValue(Promise.resolve(responseRegisterUser));
       const vuScurity = new VUSecurityApiClient('http://localhost:8087/api/vuSecurity');
-      const result = await vuScurity.registerUser('did:ethr:0x0000000000000000000000000000000000000001','VU-TEST-SDK-1','VU-TEST-SDK')
-      console.log(result);
-      expect(result).toEqual({})
-       done();
+      const result = await vuScurity.registerUser('did:ethr:0x0000000000000000000000000000000000000001','VU-TEST-SDK-1','VU-TEST-SDK');
+      expect(result).toEqual(await fetch());
+      expect(fetch).toHaveBeenCalledTimes(1);
+      done();
    }, 30000);
    
 
    it(`createVerification`, async (done) => {
+      fetch.mockReturnValue(Promise.resolve(responseCreateVerification));
       const vuScurity = new VUSecurityApiClient('http://localhost:8087/api/vuSecurity');
       const result = await vuScurity.createVerification(
          "did:ethr:0x0000000000000000000000000000000000000001",
@@ -24,15 +32,16 @@ describe("VUSecurityApiClient", () => {
           "Apple",
           "Iphone"
      )
-      expect(Object.values(result)[0]).toEqual("VU-TEST-SDK-1");
+      expect(Object.values(result)[0]).toEqual((await fetch()).userName);
+      expect(fetch).toHaveBeenCalledTimes(2);
        done();
    }, 30000);
 
-   it(`createVerification`, async (done) => {
+   it(`cancelVerification`, async (done) => {
+      fetch.mockReturnValue(Promise.resolve(responseCancelVerification));
       const vuScurity = new VUSecurityApiClient('http://localhost:8087/api/vuSecurity');
       // new registro,
-      const resultR = await vuScurity.registerUser('did:ethr:0x0000000000000000000000000000000000000002','VU-TEST-SDK-2','VU-TEST-SDK-2')    
-      console.log(resultR);
+      const resultR = await vuScurity.registerUser('did:ethr:0x0000000000000000000000000000000000000002','VU-TEST-SDK-2','VU-TEST-SDK-2');
       expect(resultR).toEqual({})
       // new vuSecurity
       const resultVU = await vuScurity.createVerification(
@@ -48,10 +57,8 @@ describe("VUSecurityApiClient", () => {
       expect(Object.values(resultVU)[0]).toEqual("VU-TEST-SDK-2");
       // cancel vuSecurity
       const result = await vuScurity.cancelVerification(Object.values(resultVU)[0],Object.values(resultVU)[1])
-         console.log('// cancel vuSecurity');
-         console.log(result);
-         
-      expect(result).toEqual("Operacion cancelada exitosamente");
+      expect(result).toEqual(await fetch());
+      expect(fetch).toHaveBeenCalledTimes(3);
        done();
    }, 90000);
 });
