ed02275303db5d34cb0acbb44082f536888417c1
farpa <arpirez.fabio@gmail.com>
2022-06-13 16:59:19 -0300

    reference comment correction

--- src/IssuerApiClient.ts
+++ src/IssuerApiClient.ts
@@ -7,15 +7,16 @@ export class IssuerApiClient {
 
 	/**
 	 *
-	 * @param baseUrl URI de la instancia de didi-ssi-identity-issuer a usar
+	 * @param baseUrl URI de la conexion con issuer-module-backend
 	 * @example "http://api.identity.example.com/"
 	 */
 	constructor(_baseUrl: string) {
 		this.baseUrl = _baseUrl;
 	}
-		/**
-		 * enviar los shareResponse
-		 */
+	
+	/**
+	* enviar los shareResponse
+	*/
 	async shareResponse(did: string, jwt: string, token : string): Promise<IShareResp>{ 
 		return authorizationToken(`${this.baseUrl}/shareResponse/${did}`,"POST",{
 			jwt,
