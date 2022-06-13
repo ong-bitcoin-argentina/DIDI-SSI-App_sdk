2c55bd7f314d233767867ba6d4edac7470533313
farpa <arpirez.fabio@gmail.com>
2022-06-13 16:44:44 -0300

    unit tests

---
+++ __tests__/Issuers/issuers_test.tsx
@@ -0,0 +1,44 @@
+import { IssuerApiClient } from '../../src/IssuerApiClient';
+import {URI_ISSUER_MODULE_BACKEND, AUTHORIZATIONTOKEN,AUTHORIZATIONTOKEN_EMPTY} from "../config/config.test.json";
+import shareResponse from "./request/addShareResponse/addShareResponse.json"; 
+import addFielJwtMandatory from "./request/addShareResponse/addFielJwtMandatory.json"; 
+require("isomorphic-fetch");
+jest.mock("node-fetch");
+
+describe("DIDI-SSI-Issuer-module-backend", () => {
+
+    it('share response with successful status', async done =>{
+    const IssuerClient = new IssuerApiClient(URI_ISSUER_MODULE_BACKEND);
+    const result = await IssuerClient.shareResponse(
+        shareResponse.did,
+        shareResponse.jwt,
+        AUTHORIZATIONTOKEN)
+    expect(result.status).toEqual('success');    
+    done();
+    })
+
+    it('Should THROW ERROR when you want to enter a jwt with space ""', async done =>{
+        const IssuerClient = new IssuerApiClient(URI_ISSUER_MODULE_BACKEND);
+        const result = await IssuerClient.shareResponse(
+            addFielJwtMandatory.did,
+            addFielJwtMandatory.jwt,
+            AUTHORIZATIONTOKEN)
+        expect(result.status).toEqual('error');
+        expect(result.data.message).toEqual('Falta el campo: jwt')
+        done();
+    
+    })
+
+    it('Should THROW ERROR when you want to enter a token with space ""', async done =>{
+        const IssuerClient = new IssuerApiClient(URI_ISSUER_MODULE_BACKEND);
+        const result = await IssuerClient.shareResponse(
+            shareResponse.did,
+            shareResponse.jwt,
+            AUTHORIZATIONTOKEN_EMPTY)
+        expect(result.status).toEqual('error');
+        expect(result.data.message).toEqual('Falta el campo: token')
+        done();
+    
+    })
+
+});
